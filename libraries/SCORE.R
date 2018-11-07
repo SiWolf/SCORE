77ef5f1c4d07eb563ebf2b9f21a0f220afbfdd81
SiWolf <silveranthony@hotmail.de>
2018-11-07 15:23:40 +0100

    Updated colours of Venn diagrams and removed fifth overlap due to an error

--- libraries/SCORE.R
+++ libraries/SCORE.R
@@ -331,9 +331,9 @@ visualization <- function(binary_table){
   v1 <- vennCounts(binary_table[1:3])
   vennDiagram(v1, circle.col = c("blue", "red", "green"))
   v2 <- vennCounts(binary_table[4:6])
-  vennDiagram(v2, circle.col = c("yellow", "grey", "cyan"))
-  v3 <- vennCounts(binary_table[1:5])
-  vennDiagram(v3, circle.col = c("blue", "red", "green", "yellow", "grey"))
+  vennDiagram(v2, circle.col = c("grey", "orange", "cyan"))
+  v3 <- vennCounts(binary_table[1:4])
+  vennDiagram(v3, circle.col = c("blue", "red", "green", "grey"))
   # UpsetR images
   upset(binary_table, nsets = 6, mainbar.y.label = "DEG Intersections", sets.x.label = "DEGs Per Tool", order.by = "freq")
 }
