3a120351038770a84718e8e8545c5e60f36ff2e1
SiWolf <silveranthony@hotmail.de>
2020-01-31 12:58:08 +0100

    (Experimental) Included new form for GeneSCF visualization and additional pathview option

---
+++ libraries/miscellaneous/visualize_kegg.R
@@ -0,0 +1,95 @@
+# -----------------------------
+# Title: visualize_kegg.R
+# Author: Silver A. Wolf
+# Last Modified: Fr, 31.01.2020
+# Version: 0.0.1
+# -----------------------------
+
+# Installers
+#install.packages("BiocManager")
+#install.packages("ggplot2")
+#BiocManager::install("pathview")
+
+# Imports
+library("ggplot2")
+library("pathview")
+
+# Functions
+
+# Better GeneSCF images
+visualize_genescf <- function(KEGG_data, GO_data){
+  for (i in 1:2) {
+    if (i == 1) {
+      name = "KEGG"
+      tmp_data <- KEGG_data[order(KEGG_data$P.value), ]
+    }
+    else {
+      name = "GO"
+      tmp_data <- GO_data[order(GO_data$P.value), ]
+    }
+    #size <- length(tmp_data[, 1])
+    tmp_data[, "Rank_in_order"] <- c(seq(1:length(tmp_data[, 1])))
+    tmp_data <- tmp_data[, cbind("Rank_in_order", "Process.name", "percentage.", "P.value")]
+    colnames(tmp_data) <- c("Rank", "Process", "Genes", "Pval")
+    tmp_data <- tmp_data[1:20, ]
+    newdata <- cbind(gsub( "~.*$", "", tmp_data[, "Process"]), tmp_data[, "Rank"])
+    colnames(newdata) <- c("IDs", "Rank")
+    png(paste("deg/pathway_analysis_", name, "/enrichment_plot.png", sep = ""), width = 1000, height = 700)
+    plot <- ggplot(tmp_data, aes(x = Rank, y = -log10(Pval), size = Genes, label = newdata[, "IDs"], fill = paste0(tmp_data[, "Rank"], ":", strtrim(tmp_data[, "Process"], 35), "...")), guide = FALSE) +
+      geom_point(colour = "#2E2E2E", shape = 21) +
+      scale_size_area(max_size = 5) +
+      labs(fill = "Rank:Process (Top 20)", title = paste("Top 20 ", name, " Hits", sep = "")) +
+      scale_x_continuous(name = "Rank", limits = c(0, 22)) +
+      scale_y_continuous(name = "-log10(Pval)", limits = c(1.1, max(-log10(tmp_data$Pval)) + 1)) +
+      #scale_size_continuous(range=c(1, 15)) +
+      geom_text(size = 5, color = "#2E2E2E", hjust = -0.1, vjust = 0, angle = 45) +
+      geom_hline(yintercept = 1.3) +
+      theme(text = element_text(size = 14), plot.title = element_text(hjust = 0.5))
+    print(plot)
+    dev.off()
+  }
+}
+
+# Visualize KEGG pathways using pathview
+visualize_pathview <- function(d1, d2, d3, d4){
+  DEGs <- d1[d1$DE..SCORE....1..Mock...Input..1..Mock...Input. != 0, ]
+  
+  c <- 1
+  fc <- c()
+  
+  for (gene in d2$V2) {
+    #gene_id <- d2[c, 1]
+    hits <- DEGs[DEGs$gene.name == gene, ]
+    pos = 1
+    if (nrow(hits) > 1) {
+      pos <- which(hits$log2FC == max(abs(hits$log2FC)) | hits$log2FC == -max(abs(hits$log2FC)))
+    }
+    fc[c] <- hits$log2FC[pos]
+    c = c + 1
+  }
+  
+  #df <- data.frame(IDs = d2[, 1], logFC = fc)
+  names(fc) <- d2[, 1]
+  
+  pathways <- sub("~.*", "", d3$Process.name)
+  
+  pv <- pathview(gene.data = fc, species = d4, pathway.id = pathways, kegg.dir = "deg/pathway_analysis_KEGG/")
+}
+
+# Main
+args <- commandArgs(TRUE)
+argument_1 = args[1]
+
+if (is.na(argument_1)) {
+  argument_1 = "rno"
+}
+
+data_1 <- read.delim(file = "deg/summary.tsv", header = TRUE)
+data_2 <- read.delim(file = "deg/pathway_analysis_KEGG/deg_gene_symbols_user_mapped.list", header = FALSE)
+data_3 <- read.delim(file = "deg/pathway_analysis_KEGG/deg_gene_symbols_KEGG_rno_functional_classification.tsv", header = TRUE)
+data_4 <- read.delim(file = "deg/pathway_analysis_GO/deg_gene_symbols_GO_all_rgd_functional_classification.tsv", header = TRUE)
+
+kegg_species = argument_1
+
+visualize_genescf(data_3, data_4)
+visualize_pathview(data_1, data_2, data_3, kegg_species)
