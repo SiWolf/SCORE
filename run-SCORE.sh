1d24c56a1d9b48607bd7c4540fe274bcdacd9ed5
SiWolf <silveranthony@hotmail.de>
2020-02-19 12:24:06 +0100

    Update to ensure PE default is not forced upon DAG creation

--- run-SCORE.sh
+++ run-SCORE.sh
@@ -1,8 +1,8 @@
 # -----------------------------
 # Title: run-SCORE.sh
 # Author: Silver A. Wolf
-# Last Modified: Mo, 17.02.2020
-# Version: 0.0.3
+# Last Modified: Wed, 19.02.2020
+# Version: 0.0.4
 # -----------------------------
 
 # Run script for SCORE
@@ -15,11 +15,11 @@
 
 ./libraries/miscellaneous/empty_results.sh full
 
-snakemake -s "SCORE_PE.snk" --forceall --dag | dot -Tsvg > deg/dag.svg
-
 if [ -z "$1" ]
 then
+	snakemake -s "SCORE_PE.snk" --forceall --dag | dot -Tsvg > deg/dag.svg
 	{ time snakemake -s "SCORE_PE.snk" -j 4 --use-conda ; } 2>&1 | tee deg/log.txt
 else
+	snakemake -s "SCORE_"$2".snk" --forceall --dag | dot -Tsvg > deg/dag.svg
 	{ time snakemake -s "SCORE_"$2".snk" -j $1 --use-conda ; } 2>&1 | tee deg/log.txt
 fi
