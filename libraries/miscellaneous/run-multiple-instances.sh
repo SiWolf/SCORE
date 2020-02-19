901adb4726530d5b146b865e9672a556f59b27b0
SiWolf <silveranthony@hotmail.de>
2020-02-19 11:40:55 +0100

    Update to running multiple analyses script

---
+++ libraries/miscellaneous/run-multiple-instances.sh
@@ -0,0 +1,29 @@
+# --------------------------------
+# Title: run-multiple-instances.sh
+# Author: Silver A. Wolf
+# Last Modified: Wed, 19.02.2020
+# Version: 0.0.3
+# --------------------------------
+
+# Script for running multiple SCORE instances in a row
+
+# Global Variables
+ANALYSES=(
+	I1
+	I2
+	I3
+	I4
+	M1
+	M2
+	M3
+	M4
+	W1
+)
+MODE="SE"
+THREADS="24"
+
+for i in "${ANALYSES[@]}"; do
+    ./libraries/miscellaneous/empty_results.sh full
+	cp config_"$i".yaml config.yaml
+	./run-SCORE.sh $THREADS $MODE
+done
