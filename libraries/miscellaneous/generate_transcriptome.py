61335ee59330d1eb10dab8f4aa8dc892e92df85a
SiWolf <silveranthony@hotmail.de>
2020-06-12 13:12:44 +0200

    Implemented pulled changes in SE mode, updated DEG environment and adjusted overall format

--- libraries/miscellaneous/generate_transcriptome.py
+++ libraries/miscellaneous/generate_transcriptome.py
@@ -1,8 +1,8 @@
 # --------------------------------
 # Title: generate_transcriptome.py
 # Author: Silver A. Wolf
-# Last Modified: Fr, 20.12.2019
-# Version: 0.0.4
+# Last Modified: Fr, 12.06.2020
+# Version: 0.0.5
 # --------------------------------
 
 import argparse
@@ -25,13 +25,13 @@ def generate_transcriptome(ANNOT, fasta, gff, ID, ffn):
 
 # Main
 if __name__ == "__main__":
-	generate_transcriptome(snakemake.params.a, snakemake.input.f, snakemake.input.g, snakemake.params.i, snakemake.output[0])
-#	parser = argparse.ArgumentParser(description = "")
-#	parser.add_argument("-a", "--annotation_feature", type = str, default = "gene", required = False, help = "Feature in gff to consider for the analysis")
-#	parser.add_argument("-f", "--fasta_genome", type = str, default = "references/REF.fasta", required = False, help = "Reference genome in fasta format")
-#	parser.add_argument("-g", "--gff_annotation_file", type = str, default = "references/REF.gff", required = False, help = "Reference annotation file in gff format")
-#	parser.add_argument("-i", "--identifier", type = str, default = "locus_tag", required = False, help = "Identifier for individual transcripts")
-#	parser.add_argument("-t", "--fasta_transcriptome", type = str, default = "references/REF.ffn", required = False, help = "Reference transcriptome in fasta format")
-#	args = parser.parse_args()
+	# parser = argparse.ArgumentParser(description = "")
+	# parser.add_argument("-a", "--annotation_feature", type = str, default = "gene", required = False, help = "Feature in gff to consider for the analysis")
+	# parser.add_argument("-f", "--fasta_genome", type = str, default = "references/REF.fasta", required = False, help = "Reference genome in fasta format")
+	# parser.add_argument("-g", "--gff_annotation_file", type = str, default = "references/REF.gff", required = False, help = "Reference annotation file in gff format")
+	# parser.add_argument("-i", "--identifier", type = str, default = "locus_tag", required = False, help = "Identifier for individual transcripts")
+	# parser.add_argument("-t", "--fasta_transcriptome", type = str, default = "references/REF.ffn", required = False, help = "Reference transcriptome in fasta format")
+	# args = parser.parse_args()
 
-#	generate_transcriptome(args.annotation_feature, args.fasta_genome, args.gff_annotation_file, args.identifier, args.fasta_transcriptome)
+	# generate_transcriptome(args.annotation_feature, args.fasta_genome, args.gff_annotation_file, args.identifier, args.fasta_transcriptome)
+	generate_transcriptome(snakemake.params.a, snakemake.input.f, snakemake.input.g, snakemake.params.i, snakemake.output[0])
